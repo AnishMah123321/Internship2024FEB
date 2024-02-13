4695ae2d4d48ddf5ea95f79e919d041b2752f012
Anish Maharjan <anish.maharjan@amniltech.com>
2024-02-13 16:01:19 +0545

    #feb 13 -migration -modal -controller -create function -view saved data -show data in table using datatable -use sweetalert

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //post
+    Route::get('/admin/post/list', [PostController::class, 'view'])->name('post.view');
+    Route::get('/admin/post/create', [PostController::class, 'create'])->name('post.create');
+    Route::post('/admin/post/create', [PostController::class, 'store'])->name('post.store');
+
 });
 
 require __DIR__.'/auth.php';
